cbc9d14fb3e7aff8ccb5f38aa442c7b9864b63ef
Binod Bhandari <binodcoder@gmail.com>
2023-04-29 15:38:06 +0545

    understanding imports and functions

--- add.py
+++ add.py
@@ -1,4 +1,10 @@
 #function to find lowest among 3 number 
+#functions: functions do some task or work. for e.g, calculations, sorting, filtering, comparing, anything
+# types of functions: user defined, pre-defined
+# user defines: user define functions.
+# per-defined: arleady defined in library
+# a and b are parameters.
+
 
 def findSmallest(a,b):
     if(a<b):
---
+++ math_1.py
@@ -0,0 +1,9 @@
+import math 
+
+
+
+#defination: details, explanation of how and why
+# witch: look for defination. 
+#36 is argument 
+result=math.sqrt(36)
+print(result)
